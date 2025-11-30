44c50776d5e3ffc6445dbcb0f0dc34023e949619
Ibrahimghali <ibrahim.elghali@outlook.com>
2025-11-30 09:15:35 +0100

    Update llm-service configuration in docker-compose and adjust Kafka environment variables in config.py

--- src/config.py
+++ src/config.py
@@ -20,12 +20,12 @@ def get_config():
         MODEL_NAME = os.getenv("MODEL_NAME", "llama3.2:3b")
         MAX_WORKERS = int(os.getenv("MAX_WORKERS", 2))
 
-        TOPIC_TO_CONSUME = os.getenv('TOPIC_TO_CONSUME', 'podcast_transcription')
-        TOPIC_TO_PRODUCE = os.getenv('TOPIC_TO_PRODUCE', 'enriched_metadata')  # Fixed: was using TOPIC_TO_CONSUME
-        KAFKA_URL = os.getenv("KAFKA_URL", "localhost")
-        KAFKA_PORT = int(os.getenv("KAFKA_PORT", 29092))
-        ACKS = int(os.getenv("ACKS", 1))
-        GROUP_ID = os.getenv('GROUP_ID', 'Nato')
+        TOPIC_TO_CONSUME = os.getenv('LLM_SERVICE_KAFKA_TOPIC_IN', 'podcast_transcription')
+        TOPIC_TO_PRODUCE = os.getenv('LLM_SERVICE_KAFKA_TOPIC_OUT', 'enriched_metadata')  # Fixed: was using TOPIC_TO_CONSUME
+        KAFKA_URL = os.getenv("LLM_SERVICE_KAFKA_URL", "localhost")
+        KAFKA_PORT = int(os.getenv("KALLM_SERVICE_KAFKA_PORTFKA_PORT", 29092))
+        ACKS = int(os.getenv("LLM_SERVICE_KAFKA_ACKS", 1))
+        GROUP_ID = os.getenv('LLM_SERVICE_KAFKA_GROUP_ID', 'Nato')
         
     # Return a single instance of the config class.
     # Due to @lru_cache, this instance will be cached in memory and reused.
